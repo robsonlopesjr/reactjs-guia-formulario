e168c3902caf4eb6a864fa3fe45b8ad8a2cf3221
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-09-27 16:51:11 -0300

    trabalhando com inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const src: React.FC = () => {
+interface FormValuesProps {
+  name: string;
+  email: string;
+}
+
+const App: React.FC = () => {
+
+  const [formValues, setFormValues] = useState({} as FormValuesProps);
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    console.log('*** handleSubmit', data);
+  };
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="name" onChange={handleInputChange} value={formValues.name} />
+
+      <input type="text" name="email" placeholder="email" onChange={handleInputChange} value={formValues.email} />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
-export default src;
+export default App;
